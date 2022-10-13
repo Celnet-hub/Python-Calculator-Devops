dd3b6cafb82e550d7124271cbd59cb91ab4b9166
Chidubem <nwabuisicelestine@gmail.com>
2022-10-13 22:47:29 +0100

    wrote a build and modified test to multiply

--- test_cal.py
+++ test_cal.py
@@ -8,7 +8,10 @@ import cal
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == calculator.add(2, 2)
+        assert 4 == cal.add(2, 2)
 
     def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
+        assert 2 == cal.subtract(4, 2)
+    
+    def test_multiplication(self):
+        assert 100 == cal.multiply(10, 10)
