2ca0457de8d474b2277873e5b6787e8ad7b23800
Chidubem <nwabuisicelestine@gmail.com>
2022-10-14 10:27:48 +0100

    modified code to pass linter part 2

--- cal.py
+++ cal.py
@@ -2,6 +2,7 @@
 Calculator library containing basic math operations.
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
 
--- test_cal.py
+++ test_cal.py
@@ -10,7 +10,6 @@ class TestCalculator:
     def test_addition(self):
         assert 4 == cal.add(2, 2)
 
-
     def test_subtraction(self):
         assert 2 == cal.subtract(4, 2)
 
